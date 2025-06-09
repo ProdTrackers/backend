c3370028c22a679907f2ac01b924bc3eb98f8a63
Ednoru <eduardochero08@gmail.com>
2025-06-09 03:15:58 -0500

    feat: add inventory relationship to Store entity and update DTO mappings

--- src/main/java/com/lockitem/storeManagement/entity/Store.java
+++ src/main/java/com/lockitem/storeManagement/entity/Store.java
@@ -1,10 +1,13 @@
 package com.lockitem.storeManagement.entity;
 
+import com.lockitem.inventoryManagement.entity.Inventory;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @Entity
@@ -17,4 +20,6 @@ public class Store {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @OneToMany(mappedBy = "store")
+    private List<Inventory> inventories;
 }
